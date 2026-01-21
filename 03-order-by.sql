23b27f63892a0f83ee6b01f87116ac81203fb5a1
Nishant Mishra <nishant.mishraa800@gmail.com>
2026-01-21 10:36:07 +0530

    Create 03-order-by.sql

---
+++ 03-order-by.sql
@@ -0,0 +1,9 @@
+-- Sorting results in ascending order
+SELECT name, salary
+FROM employees
+ORDER BY salary;
+
+-- Sorting results in descending order
+SELECT name, salary
+FROM employees
+ORDER BY salary DESC;
